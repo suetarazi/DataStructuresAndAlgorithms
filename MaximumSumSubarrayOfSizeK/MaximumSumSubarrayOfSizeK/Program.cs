9a24e61cc644484ae0e9d531d397496241ee9462
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 15:35:48 -0700

    all tests written for TDD

---
+++ MaximumSumSubarrayOfSizeK/MaximumSumSubarrayOfSizeK/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaximumSumSubarrayOfSizeK
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int findMaxSumSubArray(int k, int[] arr)
+        {
+
+            return -1;
+        }
+    }
+}
