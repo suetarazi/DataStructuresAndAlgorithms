149dfb6e08c43ea0b00d0706f4bbc3199c08b02b
Sue Tarazi <suemachtley@gmail.com>
2020-10-27 22:42:56 -0700

    all tests written for TDD

---
+++ LongestSubstringWithSameLettersAfterReplacement/LongestSubstringWithSameLettersAfterReplacement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LongestSubstringWithSameLettersAfterReplacement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int findLength(String str, int k)
+        {
+
+            return -1;
+        }
+    }
+}
