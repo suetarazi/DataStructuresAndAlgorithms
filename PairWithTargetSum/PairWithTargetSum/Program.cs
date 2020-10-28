0d59491f7810d41301da3c302219999d9d8f0738
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 10:50:53 -0700

    code done and all tests passing!

--- PairWithTargetSum/PairWithTargetSum/Program.cs
+++ PairWithTargetSum/PairWithTargetSum/Program.cs
@@ -11,8 +11,27 @@ namespace PairWithTargetSum
 
         public int[] search(int[] arr, int targetSum)
         {
-            // TODO: Write your code here
+            int leftPointer = 0;
+            int rightPointer = arr.Length -1;
+
+                while(leftPointer < rightPointer) 
+                {
+                int currentSum = arr[leftPointer] + arr[rightPointer];
+
+                    if(currentSum == targetSum)
+                    {
+                        return new int[] { leftPointer, rightPointer }; //search no further, we found our pair!
+                    }
+
+                if (targetSum > currentSum)
+                {
+                    leftPointer++;  //sum is too small, we need to increment the left pointer 
+                }
+                else
+                    rightPointer--;  //sum is too big, we need to decrement the right pointer
+                }
             return new int[] { -1, -1 };
         }
     }
 }
+
