59c2a478dd78e944a4e71029c6b9898c455380dc
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 12:31:21 -0700

    problem #2 done and all tests passing :)

--- RemoveDuplicates/RemoveDuplicates/Program.cs
+++ RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -25,16 +25,26 @@ namespace RemoveDuplicates
             return nextNonduplicate;
         }
 
+        /// <summary>
+        ///  Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+        /// </summary>
+        /// <param name="arr">input array</param>
+        /// <param name="key">key to be removed from the array</param>
+        /// <returns>length of the new array (i.e. how many numbers did we count up that we want to keep)</returns>
         public int removeKey(int[] arr, int key)
         {
-            int indexTracker = 0;
+            int nextElement = 0; //index of next element that is not our key
 
             for (int i = 0; i < arr.Length; i++)
             {
-
+                if(arr[i] != key)
+                {
+                    arr[nextElement] = arr[i];  //next element gets to jump to that point in the array
+                    nextElement++;    //increment the count of elements we want to keep
+                }
             }
 
-                return indexTracker;
+                return nextElement;
         }
     }
 }
