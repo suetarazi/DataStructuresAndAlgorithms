880214d10a10d2ec4e357ed60273bcba01a05bc9
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 15:56:36 -0700

    code done and all tests passing :)

--- NoRepeatSubstring/NoRepeatSubstring/Program.cs
+++ NoRepeatSubstring/NoRepeatSubstring/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NoRepeatSubstring
 {
@@ -9,10 +10,34 @@ namespace NoRepeatSubstring
             Console.WriteLine("Hello World!");
         }
 
+        //find the length of the longest substring which has no repeating characters
         public int findLength(string str)
         {
+            int windowStart = 0;
+            int maxSubstring = 1;
 
-            return -1;
+            Dictionary<char, int> charList = new Dictionary<char, int>();
+
+            //grow the window
+            for (int windowEnd = 0; windowEnd < str.Length; windowEnd++)
+            {
+                char rightChar = str[windowEnd];
+
+                //if the dictionary already contains rightChar, shrink the window from windowStart so we only have ONE occurance of rightChar
+                if (charList.ContainsKey(rightChar))
+                {
+                    windowStart = Math.Max(windowStart, charList[rightChar] + 1);
+                }
+
+                //insert rightChar into the dictionary
+                charList[rightChar] = windowEnd;
+
+                //remember the max substring length so far
+                maxSubstring = Math.Max(maxSubstring, windowEnd - windowStart + 1);
+            }
+
+
+            return maxSubstring;
         }
     }
 }
