acf50d9036350e082a6dd60d6566f8e88ed39d04
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 15:54:36 -0700

    all tests written for TDD

---
+++ NoRepeatSubstring/NoRepeatSubstring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NoRepeatSubstring
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int findLength(string str)
+        {
+
+            return -1;
+        }
+    }
+}
