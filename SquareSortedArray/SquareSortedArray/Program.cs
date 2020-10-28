31d88cc40aea2a5be5ec95c8d67c48c41e0478d8
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 14:34:45 -0700

    ?

--- SquareSortedArray/SquareSortedArray/Program.cs
+++ SquareSortedArray/SquareSortedArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 
 namespace SquareSortedArray
 {
@@ -9,10 +10,36 @@ namespace SquareSortedArray
             Console.WriteLine("Hello World!");
         }
 
+        /// <summary>
+        /// Brute force solution
+        /// </summary>
+        /// <param name="arr">input array</param>
+        /// <returns>new array of squared values</returns>
+        public int[] BruteForceMakeSquares(int[] arr)
+        {
+            int[] squares = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                squares[i] = arr[i] * arr[i];
+            }
+
+            Array.Sort(squares);
+
+
+            return squares;
+        }
+
+        /// <summary>
+        /// Two pointer solution: find index of the first non-negative number. Then use two pointers from that index, moving in opposite directions. At each step, whichever number squared is larger is added to the array first
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
         public int[] makeSquares(int[] arr)
         {
             int[] squares = new int[arr.Length];
-            // TODO: Write your code here
+
+
             return squares;
         }
     }
