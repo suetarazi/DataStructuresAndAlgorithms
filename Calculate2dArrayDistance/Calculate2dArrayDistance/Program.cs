a1705243739b8e077cd21f05afc84c640ff88552
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 16:25:25 -0700

    test done for TDD

---
+++ Calculate2dArrayDistance/Calculate2dArrayDistance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculate2dArrayDistance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int calculateDistance(string input)
+        {
+
+
+            return -1;
+        }
+    }
+}
