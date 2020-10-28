ee8b1b8f32882820f3aae61b67b0cc2e9fa1d120
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 10:11:24 -0700

    initial scaffold

---
+++ PairWithTargetSum/PairWithTargetSum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PairWithTargetSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] search(int[] arr, int targetSum)
+        {
+            // TODO: Write your code here
+            return new int[] { -1, -1 };
+        }
+    }
+}
