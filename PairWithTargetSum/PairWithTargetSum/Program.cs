406d2f159f4e68cf99be0e52168319e9f1df8c98
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 10:34:34 -0700

    all tests written for TDD

--- PairWithTargetSum/PairWithTargetSum/Program.cs
+++ PairWithTargetSum/PairWithTargetSum/Program.cs
@@ -2,14 +2,14 @@
 
 namespace PairWithTargetSum
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
 
-        public static int[] search(int[] arr, int targetSum)
+        public int[] search(int[] arr, int targetSum)
         {
             // TODO: Write your code here
             return new int[] { -1, -1 };
---
+++ PairWithTargetSum/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using PairWithTargetSum;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] testArr = new int[] { 1, 2, 3, 4, 6 };
+            int testTarget = 6;
+            Program p = new Program();
+            int[] actual = p.search(testArr, testTarget);
+            int[] expected = new int[] { 1, 3 };
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] testArr = new int[] { 2, 5, 9, 11 };
+            int testTarget = 11;
+            Program p = new Program();
+            int[] actual = p.search(testArr, testTarget);
+            int[] expected = new int[] { 0, 2 };
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
