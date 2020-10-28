7b22a01c8d6d954dbb2b0496f0915f444db04a77
Sue Tarazi <suemachtley@gmail.com>
2020-10-27 22:15:16 -0700

    all tests written for TDD

---
+++ FruitsIntoBaskets/FruitsIntoBaskets/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FruitsIntoBaskets
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int findLength(char[] arr)
+        {
+
+            return -1;
+        }
+    }
+}
