932c0aa720dfcc260a133dc72a1e008267482c42
Sue Tarazi <suemachtley@gmail.com>
2020-10-28 13:23:13 -0700

    All tests written for TDD

---
+++ SquareSortedArray/SquareSortedArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SquareSortedArray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int[] makeSquares(int[] arr)
+        {
+            int[] squares = new int[arr.Length];
+            // TODO: Write your code here
+            return squares;
+        }
+    }
+}
---
+++ SquareSortedArray/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using SquareSortedArray;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] testArr = new int[] { -2, -1, 0, 2, 3 };
+            Program p = new Program();
+            int[] actual = p.makeSquares(testArr);
+            int[] expected = new int[] { 0, 1, 4, 4, 9 };
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] testArr = new int[] { -3, -1, 0, 1, 2 };
+            Program p = new Program();
+            int[] actual = p.makeSquares(testArr);
+            int[] expected = new int[] { 0, 1, 1, 4, 9 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
