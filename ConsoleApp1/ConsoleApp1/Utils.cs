d2aac43ef650e9d10a5d59c06b358e5f16394351
Sue Tarazi <suemachtley@gmail.com>
2020-10-29 17:04:20 -0700

    basic scaffold and initial code

---
+++ ConsoleApp1/ConsoleApp1/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Utils
+    {
+        // this function helps candidates to read the JSON input.
+        public static string getURL()
+        {
+            using (StreamReader r = new StreamReader("userInput.json"))
+            {
+                string json = r.ReadToEnd();
+                List<string> items = JsonConvert.DeserializeObject<List<string>>(json);
+                return items[0];
+            }
+
+        }
+    }
+}
