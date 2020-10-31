5f1f13146db13c7809d6f932914f6dc0b0688449
Sue Tarazi <suemachtley@gmail.com>
2020-10-30 18:48:01 -0700

    trying to use Regex instead of string.split. Tests written for TDD

--- HowManyWords/XUnitTestProject1/UnitTest1.cs
+++ HowManyWords/XUnitTestProject1/UnitTest1.cs
@@ -38,5 +38,39 @@ namespace XUnitTestProject1
             Assert.Equal(expected, actual);
 
         }
+
+        [Fact]
+        public void Test4()
+        {
+            string testString = "How many eggs are in a half-dozen, 13?";
+            Program p = new Program();
+            int actual = p.howMany2(testString);
+            int expected = 7;
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            string testString = "He is a good programmer, he won 865 competitions, but sometimes he dont. What do you think? All test-cases should pass. Done-done?";
+            Program p = new Program();
+            int actual = p.howMany2(testString);
+            int expected = 21;
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            string testString = "jds dsaf lkdf kdsa fkldsf, adsbf ldka ads? asd bfdal ds bf[l. akf dhj ds 878 dwa WE DE 7475 dsfh ds RAMU 748 dj.";
+            Program p = new Program();
+            int actual = p.howMany2(testString);
+            int expected = 21;
+            Assert.Equal(expected, actual);
+
+        }
+
     }
 }
