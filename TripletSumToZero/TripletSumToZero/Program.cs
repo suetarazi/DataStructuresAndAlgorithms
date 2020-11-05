f190de9e267e2f937f36f80fcd3ac0b1447025ef
Sue Tarazi <suemachtley@gmail.com>
2020-11-04 16:45:32 -0800

    method signature and return created

---
+++ TripletSumToZero/TripletSumToZero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TripletSumToZero
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public List<int[]> searchTriplets(int[] arr)
+        {
+            List<int[]> triplets = new List<int[]>();
+            // TODO: Write your code here
+            return triplets;
+        }
+    }
+}
